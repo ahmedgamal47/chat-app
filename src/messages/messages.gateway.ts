63149735bbf78e560a512bb1dc8dd5235b7f7b66
Ahmed Gamal <ahmedgamal47.ag@gmail.com>
2022-11-04 01:20:25 +0200

    init

---
+++ src/messages/messages.gateway.ts
@@ -0,0 +1,65 @@
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { MessagesService } from './messages.service';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { UpdateMessageDto } from './dto/update-message.dto';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class MessagesGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  constructor(private readonly messagesService: MessagesService) {}
+
+  @WebSocketServer()
+  server: Server;
+
+  @SubscribeMessage('createMessage')
+  create(@MessageBody() data: any) {
+    this.server.emit('recieveMessage', data);
+    return data;
+  }
+
+  @SubscribeMessage('findAllMessages')
+  findAll() {
+    return this.messagesService.findAll();
+  }
+
+  @SubscribeMessage('findOneMessage')
+  findOne(@MessageBody() id: number) {
+    return this.messagesService.findOne(id);
+  }
+
+  @SubscribeMessage('updateMessage')
+  update(@MessageBody() updateMessageDto: UpdateMessageDto) {
+    return this.messagesService.update(updateMessageDto.id, updateMessageDto);
+  }
+
+  @SubscribeMessage('removeMessage')
+  remove(@MessageBody() id: number) {
+    return this.messagesService.remove(id);
+  }
+
+  afterInit(server: Server) {
+    console.log('init');
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Client disconnected: ${client.id}`);
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    console.log(`Client connected: ${client.id}`);
+    return { id: client.id };
+  }
+}
